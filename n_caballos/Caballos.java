85770932d6e706659a98f8fceff729aea36f1508
Vitor-95 <vcp256@gmail.com>
2020-11-23 01:16:45 -0400

    IMPLEMENTACION DE LA INTERFAZ GRAFICA A LOS PROBLEMAS DE LAS N-REINAS Y LOS N-CABALLOS

--- n_caballos/Caballos.java
+++ n_caballos/Caballos.java
@@ -1,23 +1,165 @@
 package n_caballos;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
-public class Caballos {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Caballos extends JFrame{
+
+    private static final long serialVersionUID=1;
     private int NUM_CABALLOS;
     private int[] solution;
-
+    private final static int SIZE=600;
+    private JPanel panelBoard;
+    private JPanel panelResult;
+    private List<int[]> listSolutions=null;
+    private JButton[][] btnCells;
+    private int sizeSol;
+    private int sol;
     public Caballos(int NUM_CABALLOS) {
         this.NUM_CABALLOS = NUM_CABALLOS;
         solution = new int[NUM_CABALLOS];
         init();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(SIZE,SIZE);
+        
+        BorderLayout gestorLayout=new BorderLayout();
+        
+        setLayout(gestorLayout);
+
+        panelBoard=new JPanel();
+        panelResult=new JPanel();
+        add(BorderLayout.NORTH,getOptions());
+        add(BorderLayout.SOUTH,getResult());
+        add(BorderLayout.CENTER,getBoard());
+        
+
+        setLocationRelativeTo(this);
+        setVisible(true);
+    }
+    public void resetCaballos(int NUM_CABALLOS) {
+            this.NUM_CABALLOS = NUM_CABALLOS;
+            solution = new int[NUM_CABALLOS];
+            sol=0;
+            init();
+    }
+    
+    public JPanel getBoard(){
+        panelBoard.removeAll();
+        panelBoard.revalidate();
+        panelBoard.repaint();
+        btnCells=new JButton[NUM_CABALLOS][NUM_CABALLOS];
+        GridLayout gestor=new GridLayout(NUM_CABALLOS,NUM_CABALLOS);
+        panelBoard.setLayout(gestor);
+        for(int i=0;i<NUM_CABALLOS;i++){
+            for(int j=0;j<NUM_CABALLOS;j++){
+                JButton cell=new JButton("");
+                cell.setBackground(Color.WHITE);
+                if((i+j)%2==0){
+                    cell.setBackground(Color.BLACK);
+                }
+                cell.setEnabled(false);
+                btnCells[i][j]=cell;
+                panelBoard.add(cell);
+            }
+        }
+        panelBoard.revalidate();
+        panelBoard.repaint();
+        return panelBoard;
+    }
+    public JPanel getOptions(){
+        JPanel panelNorth=new JPanel();
+        FlowLayout gestor=new FlowLayout();
+        panelNorth.setLayout(gestor);
+        JLabel lbnumCaballos=new JLabel("NUMERO DE CABALLOS: ");
+        panelNorth.add(lbnumCaballos);
+        JTextField txtNumCaballos=new JTextField(3);
+        panelNorth.add(txtNumCaballos);
+        JButton btnGo=new JButton("GO");
+        btnGo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String strNumCaballos=txtNumCaballos.getText();
+                NUM_CABALLOS=Integer.parseInt(strNumCaballos);
+                resetCaballos(NUM_CABALLOS);
+                getBoard();
+                listSolutions=new LinkedList<>();
+                searchSolution();
+                sizeSol=listSolutions.size();
+                getResult();
+                paintSolution(listSolutions.get(0));
+            }
+        });
+        panelNorth.add(btnGo);
+        return panelNorth;
+    }
+    public void paintSolution(int[] s){
+        for(int i=0;i<s.length;i++){
+            btnCells[s[i]][i].setText("X");
+        }
+    }
+    public JPanel getResult(){
+        //panelResult=new JPanel();
+        panelResult.removeAll();
+        panelResult.revalidate();
+        panelResult.repaint();
+        
+        JLabel lbnmsjCantidad=new JLabel("Cant Solucion: ");
+        //JLabel lbnCantidad=new JLabel("0");
+        JLabel lbnCantidad=new JLabel(Integer.toString(sizeSol));
+        JButton btnPreview=new JButton("<<");
+        btnPreview.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getBoard();
+                sol=sol-1;
+                paintSolution(listSolutions.get(sol));
+                getResult();
+            }
+        });
+        JLabel  lbnNumSolCurrent=new JLabel(Integer.toString(sol+1));
+        JButton btnNext= new JButton(">>");
+        btnNext.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getBoard();
+                sol=sol+1;
+                paintSolution(listSolutions.get(sol));
+                getResult();
+            }
+        });
+        panelResult.add(lbnmsjCantidad);
+        panelResult.add(lbnCantidad);
+        panelResult.add(btnPreview);
+        panelResult.add(lbnNumSolCurrent);
+        panelResult.add(btnNext);
+        panelResult.revalidate();
+        panelResult.repaint();
+
+
+        return panelResult;
     }
 
+
+
+    
     public void init() {
         for (int i = 0; i < solution.length; i++) {
             solution[i] = -1;
         }
     }
+    
 
     public void searchSolution() {
         init();
@@ -33,11 +175,12 @@ public class Caballos {
                 String strSol = Arrays.toString(solucion);
                if (valid && (caballo ==NUM_CABALLOS - 1)) {
                     System.out.println(strSol + " SOLUCION");
+                    int[] sClone =solucion.clone();
+                    listSolutions.add(sClone);
                 }
 
                 
-                 /*System.out.println(strSol + " " + (valid ? "SOL PARCIAL " : "") + (valid &&
-                 (caballo == NUM_CABALLOS - 1) ? "SOLUTION" : ""));*/
+                 
                  
                 if (valid) {
 
@@ -48,19 +191,13 @@ public class Caballos {
         } else {
 
         }
+        sizeSol=listSolutions.size();
         return success;
     }
 
 
     public boolean isValid(int[] solution, int caballo) {
-        //boolean ok = true;
-        /*for (int i = 0; i < caballo; i++) {
-            if (solution[caballo]==solution[i]-2||solution[caballo]==solution[i]-1||
-                solution[caballo]==solution[i]+2||solution[caballo]==solution[i]+1){
-                ok = false;
-                break;
-            }
-        }*/
+       
             if(caballo-1>=0){
                 if (solution[caballo]==solution[caballo-1]-2||solution[caballo]==solution[caballo-1]+2){
                     return false;
@@ -77,6 +214,6 @@ public class Caballos {
 
     public static void main(String[] args) {
         Caballos caballo = new Caballos(4);
-        caballo.searchSolution();
+        //caballo.searchSolution();
     }
 }
--- n_reinas/Reinas.java
+++ n_reinas/Reinas.java
@@ -1,18 +1,160 @@
 package n_reinas;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
-public class Reinas {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Reinas extends JFrame{
+    
+    private static final long serialVersionUID=1;
 
     private int NUM_REINAS;
     private int[] solution;
-
+    private final static int SIZE=600;
+    private JPanel panelBoard;
+    private JPanel panelResult;
+    private List<int[]> listSolutions=null;
+    private JButton[][] btnCells;
+    private int sizeSol;
+    private int sol;
     public Reinas(int NUM_REINAS) {
         this.NUM_REINAS = NUM_REINAS;
         solution = new int[NUM_REINAS];
+        //sizeSol=listSolutions.size();
         init();
-        // String strArray = Arrays.toString(solution);
-        // System.out.println(strArray);
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(SIZE,SIZE);
+        
+        BorderLayout gestorLayout=new BorderLayout();
+        
+        setLayout(gestorLayout);
+
+        panelBoard=new JPanel();
+        panelResult=new JPanel();
+        add(BorderLayout.NORTH,getOptions());
+        add(BorderLayout.SOUTH,getResult());
+        add(BorderLayout.CENTER,getBoard());
+        
+
+        setLocationRelativeTo(this);
+        setVisible(true);
+
+    }
+    public void resetReinas(int NUM_REINAS) {
+        this.NUM_REINAS = NUM_REINAS;
+        solution = new int[NUM_REINAS];
+        sol=0;
+        init();
+    }
+
+    public JPanel getBoard(){
+        panelBoard.removeAll();
+        panelBoard.revalidate();
+        panelBoard.repaint();
+        btnCells=new JButton[NUM_REINAS][NUM_REINAS];
+        GridLayout gestor=new GridLayout(NUM_REINAS,NUM_REINAS);
+        panelBoard.setLayout(gestor);
+        for(int i=0;i<NUM_REINAS;i++){
+            for(int j=0;j<NUM_REINAS;j++){
+                JButton cell=new JButton("");
+                cell.setBackground(Color.WHITE);
+                if((i+j)%2==0){
+                    cell.setBackground(Color.BLACK);
+                }
+                cell.setEnabled(false);
+                btnCells[i][j]=cell;
+                panelBoard.add(cell);
+            }
+        }
+        panelBoard.revalidate();
+        panelBoard.repaint();
+        return panelBoard;
+    }
+
+    public JPanel getOptions(){
+        JPanel panelNorth=new JPanel();
+        FlowLayout gestor=new FlowLayout();
+        panelNorth.setLayout(gestor);
+        JLabel lbnumReinas=new JLabel("NUMERO DE REINAS: ");
+        panelNorth.add(lbnumReinas);
+        JTextField txtNumReinas=new JTextField(3);
+        panelNorth.add(txtNumReinas);
+        JButton btnGo=new JButton("GO");
+        btnGo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String strNumReinas=txtNumReinas.getText();
+                NUM_REINAS=Integer.parseInt(strNumReinas);
+                resetReinas(NUM_REINAS);
+                getBoard();
+                listSolutions=new LinkedList<>();
+                searchSolution();
+                sizeSol=listSolutions.size();
+                getResult();
+                paintSolution(listSolutions.get(0));
+            }
+        });
+        panelNorth.add(btnGo);
+        return panelNorth;
+    }
+    
+    public void paintSolution(int[] s){
+        for(int i=0;i<s.length;i++){
+            btnCells[s[i]][i].setText("X");
+        }
+    }
+    public JPanel getResult(){
+        //panelResult=new JPanel();
+        panelResult.removeAll();
+        panelResult.revalidate();
+        panelResult.repaint();
+        
+        JLabel lbnmsjCantidad=new JLabel("Cant Solucion: ");
+        //JLabel lbnCantidad=new JLabel("0");
+        JLabel lbnCantidad=new JLabel(Integer.toString(sizeSol));
+        JButton btnPreview=new JButton("<<");
+        btnPreview.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getBoard();
+                sol=sol-1;
+                paintSolution(listSolutions.get(sol));
+                getResult();
+            }
+        });
+        JLabel  lbnNumSolCurrent=new JLabel(Integer.toString(sol+1));
+        JButton btnNext= new JButton(">>");
+        btnNext.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getBoard();
+                sol=sol+1;
+                paintSolution(listSolutions.get(sol));
+                getResult();
+            }
+        });
+        panelResult.add(lbnmsjCantidad);
+        panelResult.add(lbnCantidad);
+        panelResult.add(btnPreview);
+        panelResult.add(lbnNumSolCurrent);
+        panelResult.add(btnNext);
+        panelResult.revalidate();
+        panelResult.repaint();
+
+
+        return panelResult;
     }
 
     public void init() {
@@ -24,6 +166,8 @@ public class Reinas {
     public void searchSolution() {
         init();
         backtracking(solution, 0);
+        
+        //System.out.println("CANTIDAD: "+sizeSol);
     }
 
     public boolean backtracking(int[] solucion, int reina) {
@@ -36,7 +180,9 @@ public class Reinas {
                 boolean valid = isValid(solution, reina);
                 String strSol = Arrays.toString(solucion);
                 if(valid&&(reina==NUM_REINAS-1)){
-                    System.out.println(strSol+" SOLUCION");
+                    //System.out.println(strSol+" SOLUCION");
+                    int[] sClone =solucion.clone();
+                    listSolutions.add(sClone);
                 }
                 
                 /*
@@ -51,11 +197,11 @@ public class Reinas {
         } else {
 
         }
+        sizeSol=listSolutions.size();
         return success;
     }
 
-    // ESTUDIAR Y EXPLICAR LA SIGUIENTE CLASE COMO ES QUE SE DETERMINA
-    // SI LA RESTRINCCION SE CUMPLE O NO (FILA Y DIAGONALES)
+    
     public boolean isValid(int[] solution, int reina) {
         boolean ok = true;
         for (int i = 0; i < reina; i++) {
@@ -69,6 +215,6 @@ public class Reinas {
 
     public static void main(String[] args) {
         Reinas reina = new Reinas(4);
-        reina.searchSolution();
+        //reina.searchSolution();
     }
 }
